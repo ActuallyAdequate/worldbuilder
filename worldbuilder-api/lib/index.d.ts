e4f2d20c962c2bd4b8fc1c35fb9301ebe8522fa0
ActuallyAdequate <actuallyadequate@gmail.com>
2023-09-17 12:53:28 +1000

    :purge

--- worldbuilder-api/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare abstract class Editor {
-    name: string;
-    constructor(name: string);
-    abstract OnOpen(): void;
-    abstract OnClose(): void;
-    abstract OnRender(): HTMLElement;
-}
--- worldbuilder-plugins/worldbuilder-plugin-sample/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Editor } from "../node_modules/@actuallyadequate/worldbuilder-api/lib/index";
-export default class HelloWorldEditor extends Editor {
-    constructor();
-    OnOpen(): void;
-    OnClose(): void;
-    OnRender(): HTMLElement;
-}
